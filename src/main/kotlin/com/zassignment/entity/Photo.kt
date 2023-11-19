764be2f598cb946aa77e84d600883b2a36666bdd
KosukeIba <kosuke.iba@tcs.com>
2023-11-19 19:29:56 +0900

    feat:new Entity to interact with firestore instead of datastore

--- src/main/kotlin/com/zassignment/entity/Photo.kt
+++ src/main/kotlin/com/zassignment/entity/Photo.kt
@@ -1,15 +1,33 @@
 package com.zippractice1.entity
 
-import com.google.cloud.spring.data.datastore.core.mapping.Entity
+// import com.google.cloud.spring.data.datastore.core.mapping.Entity
+import lombok.Getter
+import lombok.Setter
 import org.springframework.data.annotation.Id
 import java.sql.Timestamp
 
-@Entity
-data class Photo (
-    @Id
-    var id: String? = null,
-    var uri: String? = null,
-    var label: String? = null,
-    var ownerId: String? = null,
-    var timestamp: com.google.cloud.Timestamp?
-)
+// this is for interaction with datastore not for firestore
+//@Entity
+//data class Photo (
+//    @Id
+//    var id: String? = null,
+//    var uri: String? = null,
+//    var label: String? = null,
+//    var ownerId: String? = null,
+//    var timestamp: com.google.cloud.Timestamp?
+//)
+
+@Getter
+@Setter
+public class Photo {
+
+    var id : String = "";
+    var uri : String = "";
+    var label : String = "";
+    var ownerId : String = "";
+    var timestamp : com.google.cloud.Timestamp? = null
+
+
+
+}
+
